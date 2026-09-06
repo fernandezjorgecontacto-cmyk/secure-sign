57304290c505ba5dfb63d520ea0ed87e330ea647
fernandezjorgecontacto-cmyk <fernandezjorgecontacto@gmail.com>
2026-09-05 21:20:58 -0300

    Add files via upload

---
+++ secure-sign-folio-main 2/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import viteReact from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tsConfigPaths(),
+    tanstackStart(),
+    viteReact(),
+    tailwindcss(),
+  ],
+});
